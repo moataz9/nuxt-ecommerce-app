d7da89e382fe0010c36e743c956860169fe7c5fe
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-01-14 00:12:16 +0200

    design: for footer

--- components/Main/Footer.vue
+++ components/Main/Footer.vue
@@ -1,48 +1,57 @@
 <template>
-  <footer class="app-bg-color mt-4">
+  <footer class="app-bg-color mt-4 py-4">
     <div class="container-xxl">
       <b-container fluid>
-        <b-row class="py-4">
+        <b-row class="pb-4">
           <b-col cols="12">
             <b-img src="/images/logo.svg" alt="logo"></b-img>
           </b-col>
         </b-row>
-        <b-row class="row-cols-xl-4">
+        <b-row class="row-cols-1 row-cols-md-2 row-cols-xl-4">
           <!--  -->
           <b-col>
-            <h3 class="font-weight-bold">information</h3>
-            <p>All rights reserved © 2015 online store AIKEA.BY Belarus.</p>
-            <address>
-              UNP 191828159 IE Pareiko V.S., registration number in the
-              commercial register 158299
-            </address>
-            <p>Privacy Policy</p>
+            <div class="pr-5">
+              <h4 class="footer-title">information</h4>
+              <div class="footer-info">
+                <p>All rights reserved © 2015 online store AIKEA.BY Belarus.</p>
+                <address>
+                  UNP 191828159 IE Pareiko V.S., registration number in the
+                  commercial register 158299
+                </address>
+                <p class="mt-4">Privacy Policy</p>
+              </div>
+            </div>
           </b-col>
           <!--  -->
           <b-col>
-            <h3 class="font-weight-bold">Menu</h3>
-            <ul
-              v-for="(linkArr, i) in footerLinks"
-              :key="i"
-              class="pl-0 list-unstyled"
-            >
-              <li
-                v-for="(linkInfo, index) in linkArr"
-                :key="index"
-                class="d-inline-block"
+            <div class="pr-5">
+              <h4 class="footer-title">Menu</h4>
+              <ul
+                v-for="(linkArr, i) in footerLinks"
+                :key="i"
+                class="footer-links"
               >
-                <nuxt-link :to="{ name: linkInfo.routeName }">
-                  {{ linkInfo.name }}
-                </nuxt-link>
-              </li>
-            </ul>
+                <li
+                  v-for="(linkInfo, index) in linkArr"
+                  :key="index"
+                  class="footer-links_link"
+                >
+                  <nuxt-link :to="{ name: linkInfo.routeName }">
+                    {{ linkInfo.name }}
+                  </nuxt-link>
+                </li>
+              </ul>
+            </div>
           </b-col>
+          <!--  -->
           <b-col>
-            <h3 class="font-weight-bold">Contacts</h3>
-            <ul class="pl-0 list-unstyled">
-              <li>Minsk</li>
+            <h4 class="footer-title">Contacts</h4>
+            <ul class="pl-0 list-unstyled footer-info">
               <li>
-                <address>St. Pervomaiskaya, D. 1, Apt. 43</address>
+                <address>
+                  Minsk <br />
+                  St. Pervomaiskaya, D. 1, Apt. 43
+                </address>
               </li>
               <li>
                 <a href="tel:+"> + 375 434 847 28 84 </a>
@@ -50,14 +59,15 @@
               <li>
                 <a href="tel:+"> + 375 448 473 09 48 </a>
               </li>
-              <li>
+              <li class="mt-5">
                 <a href="mailto:#"> ikeaekspress@gmail.com </a>
               </li>
             </ul>
           </b-col>
+          <!--  -->
           <b-col>
-            <ul class="list-unstyled pl-0">
-              <li>
+            <ul class="list-unstyled pl-0 d-flex justify-content-end flex-column h-100">
+              <li class="my-3">
                 <b-img src="/images/social/instegram.png"></b-img>
               </li>
             </ul>
@@ -104,4 +114,38 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+$app-color: #FF9900 !default;
+
+.footer {
+  &-title {
+    font-weight: bold;
+    font-size: 1rem;
+  }
+  &-info {
+    font-size: 14px;
+  }
+  &-links {
+    font-size: 14px;
+    list-style: none;
+    padding-left: 0;
+    &_link {
+      display: inline-block;
+      line-height: 1rem;
+      &:not(:last-of-type) {
+        border-right: 1px solid #ccc;
+        padding-right: 5px;
+        margin-right: 5px;
+      }
+      a {
+        color: #000;
+        text-decoration: none;
+        transition: color .1s ease-in-out;
+        &:hover {
+          color: $app-color;
+        }
+      }
+    }
+  }
+}
+</style>
