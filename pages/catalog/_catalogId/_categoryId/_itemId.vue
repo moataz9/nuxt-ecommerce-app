62353363561b9a316200abc6663a20dd8b3a9cd3
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-01-20 01:50:04 +0200

    design: single item page

--- pages/catalog/_catalogId/_categoryId/_itemId.vue
+++ pages/catalog/_catalogId/_categoryId/_itemId.vue
@@ -42,7 +42,56 @@
               </b-col>
             </b-row>
           </b-col>
-          <b-col class="border"> </b-col>
+          <b-col class="border">
+            <h4 class="font-weight-bold">{{ itemName }}</h4>
+            <p>
+              <span class="font-weight-bold">Art. {{ itemArt }}</span>
+              <span class="app-secondary-color">/ On Order as {{ lastOrderDate }}</span>
+            </p>
+            <p class="app-secondary-color">{{ description }}</p>
+            <h4 class="h3 font-weight-bold">{{ price }}</h4>
+            <b-btn-group class="d-block mt-3">
+              <b-button
+                variant="warning"
+                class="custom-btn rounded-pill text-white font-weight-bold el-bg-color border-0 w-75 d-inline-block py-3"
+                >Add to Cart +</b-button
+              >
+              <b-button
+                variant="warning"
+                class="custom-btn rounded-circle bg-transparent app-shadow p-3 border-0 d-inline-block ml-2"
+                ><b-img src="/icons/star.svg"></b-img
+              ></b-button>
+              <b-button
+                variant="warning"
+                class="custom-btn rounded-circle bg-transparent app-shadow p-3 border-0 d-inline-block ml-2"
+                ><b-img src="/icons/highRate.svg"></b-img
+              ></b-button>
+            </b-btn-group>
+            <p class="my-5 app-secondary-color">{{ fullDescription }}</p>
+            <p class="border-top py-2">
+              <span class="font-weight-bold">Color :</span> {{ itemColor }} &rightarrow;
+            </p>
+            <p class="border-top py-2 font-weight-bold">
+              <a href="#"> Read more about product &rightarrow; </a>
+            </p>
+          </b-col>
+        </b-row>
+        <b-row class="my-4">
+          <b-col>
+            <h2 class="h1 font-weight-bold">Also ordered with this product</h2>
+          </b-col>
+        </b-row>
+        <b-row cols-xl="4">
+          <b-col v-for="(item, index) in popularItems()" :key="index">
+            <AppItem
+              :imagePath="item.image"
+              :itemName="item.name"
+              :description="item.description"
+              :price="item.price.priceValue"
+              :discount="item.discount"
+              :currency="item.currency"
+            />
+          </b-col>
         </b-row>
       </b-container>
     </div>
@@ -50,7 +99,40 @@
 </template>
 
 <script>
-export default {}
+import items from '@/data/items.json'
+
+export default {
+  data() {
+    return {
+      items,
+      itemName: 'VINTER 2020 VINTER 2020',
+      itemArt: '890.321.44',
+      lastOrderDate: '4/04/2022',
+      description:
+        'Jar with lid and tray, 5 pcs., glass stopper jars and boxes will help organize convenient storage.',
+      fullDescription:
+        'These jars and boxes will help organize convenient storage of various small things. You can move the containers at any time if you need the items stored in them, not only in the bathroom.',
+      price: '$62,392.31',
+      itemColor: 'Black',
+    }
+  },
+  methods: {
+    popularItems() {
+      return this.items.filter(item => {
+        return item.popular === true
+      })
+    },
+  },
+}
 </script>
 
-<style></style>
+<style scoped lang="scss">
+$app-color: #ff9900 !default;
+.custom-btn {
+  &:hover,
+  &:active,
+  &:focus {
+    background: $app-color;
+  }
+}
+</style>
