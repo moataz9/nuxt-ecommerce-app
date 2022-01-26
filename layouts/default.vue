add3716b0165a5e043082cd2ec5436a5d65cef3a
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-01-26 10:09:47 +0200

    finish with mobile components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-    <MainNavbar />
-    <MainSearch />
+    <MainNavbar v-if="!mobile" />
+    <MobileNavbar v-else/>
+    <MainSearch v-if="!mobile" />
+    <MobileWidgets v-else />
+
     <AppSidebar />
+    <div v-if="mobile && $route.name != 'index'" style="height: 4.7rem;"></div>
     <Nuxt />
     <MainFooter />
+    <div v-if="mobile" style="height: 3rem;"></div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      mobile: false,
+    }
+  },
+  mounted() {
+    this.mobile = this.$screens.mobile()
+  },
+}
 </script>
 
 <style></style>
