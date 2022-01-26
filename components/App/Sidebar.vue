e02498d5cbb3ef11a8d59d93fe966db15becf7f8
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-01-26 09:04:12 +0200

    content: app internals links

--- components/App/Sidebar.vue
+++ components/App/Sidebar.vue
@@ -1,23 +1,37 @@
 <template>
-  <b-sidebar
-    id="app-sidebar"
-    title="Sidebar"
-    body-class="bg-white"
-    header-class="bg-white"
-  >
-    <span class="blur-effect"></span>
+  <b-sidebar id="app-sidebar" body-class="bg-white" header-class="bg-white" class="pl-5">
+    <template #header="{ hide }">
+      <div>
+        <span @click="hide" class="blur-effect"></span>
+        <nuxt-link to="/" class="h4 font-weight-bold text-uppercase">
+          Nuxt e-commerce
+        </nuxt-link>
+        <button @click="hide" class="border-0 bg-transparent ml-2 h2 pt-1">&times;</button>
+      </div>
+    </template>
     <div class="px-3 py-2">
-      <p>
-        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-        consectetur ac, vestibulum at eros.
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
-        perferendis soluta quidem quaerat nam ullam debitis ut odio, eaque
-        reprehenderit officiis quia rerum voluptatum magni pariatur blanditiis
-        quo id unde.
-      </p>
+      <ul class="list-unstyled">
+        <li v-for="(link, index) in sidebarLinks" :key="index" class="mt-3">
+          <nuxt-link :to="link.route" class="h5 font-weight-bold">{{ link.route }}</nuxt-link>
+        </li>
+      </ul>
     </div>
   </b-sidebar>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      sidebarLinks: [
+        { name: 'Catalog', route: 'catalog' },
+        { name: 'In Stocks', route: 'in_stock' },
+        { name: 'Discounts', route: 'discounts' },
+        { name: 'Popular', route: 'popular' },
+        { name: 'Services', route: 'services' },
+        { name: 'Login', route: 'login' },
+      ],
+    }
+  },
+}
+</script>
