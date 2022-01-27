0c08ee1529238d349a8c6271f2001997758b7b29
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-01-27 07:39:58 +0200

    responsive: mobile navbar and widgets

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <MainNavbar v-if="!mobile" />
-    <MobileNavbar v-else/>
-    <MainSearch v-if="!mobile" />
-    <MobileWidgets v-else />
+    <MainNavbar class="less-md-none" v-if="!mobile" />
+    <MobileNavbar class="more-md-none" v-else/>
+    <MainSearch class="less-md-none" v-if="!mobile" />
+    <MobileWidgets class="more-md-none" v-else />
 
     <AppSidebar />
     <div v-if="mobile && $route.name != 'index'" style="height: 4.7rem;"></div>
@@ -26,4 +26,15 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+@media (max-width: 720px) {
+  .less-md-none {
+    display: none;
+  }
+}
+@media (min-width: 720px) {
+  .more-md-none {
+    display: none;
+  }
+}
+</style>
